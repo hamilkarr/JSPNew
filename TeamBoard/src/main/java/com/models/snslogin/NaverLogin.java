7be746b78b05b459da9e8a24364f6a1a926af8f5
hamilkarr <12uclock@gmail.com>
2021-11-18 18:14:39 +0900

    카카오 로그인 개인정보 받는중

--- TeamBoard/src/main/java/com/models/snslogin/NaverLogin.java
+++ TeamBoard/src/main/java/com/models/snslogin/NaverLogin.java
@@ -84,7 +84,7 @@ public class NaverLogin extends SocialLogin {
 			} else { // 오류 발생시
 				throw new Exception((String)json.get("error_description"));
 			}
-		}		
+		}
 		
 		return accessToken;
 	}
